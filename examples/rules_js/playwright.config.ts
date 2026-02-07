4930437a787fee590dfdea89b031f7d1154ca9e2
Tyler Breisacher <tyler.breisacher@flyzipline.com>
2026-02-07 14:13:07 -0800

    I think I have things more or less working!

--- examples/rules_js/playwright.config.ts
+++ examples/rules_js/playwright.config.ts
@@ -50,14 +50,14 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
   ],
 });
